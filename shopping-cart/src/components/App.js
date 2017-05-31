5d90a0f8f7344e0eba230ca2117c6657533f5e7f
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-31 15:40:24 -0400

    finished coding

---
+++ shopping-cart/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Cart from '../containers/Cart'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Cart />
+    );
+  }
+}
+
+export default App
